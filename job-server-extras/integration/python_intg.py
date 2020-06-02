501fe4fcb2c318ec5f43cb63bc401768681132c2
congyh <yihao.cong@outlook.com>
2020-06-02 16:57:44 +0800

    Init Python integration.

---
+++ job-server-extras/integration/python_intg.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# TODO: python2 compatible.
+
+import json
+
+from urllib import request, parse
+
+url = 'http://10.198.47.106:8090/jobs'
+
+queryParams = {
+    'appName': 'join-framework',
+    'classPath': 'spark.jobserver.RunSqlWithOutputJob',
+    'context': 'join-framework-context-0',
+    'sync': 'true'
+}
+
+querystring = parse.urlencode(queryParams)
+
+form = {
+    "sql": "select 1"
+}
+
+# Make a POST request and read the response
+u = request.urlopen(url + '?' + querystring, data=json.dumps(form).encode("utf-8"))
+resp = u.read()
+print(resp)
