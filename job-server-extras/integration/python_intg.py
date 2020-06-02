70e76a7320b981c824c2ca6033388b0e81552b25
congyh <yihao.cong@outlook.com>
2020-06-02 19:21:49 +0800

    Modeling request operation.

--- job-server-extras/integration/python_intg.py
+++ job-server-extras/integration/python_intg.py
@@ -1,26 +1,149 @@
 #!/usr/bin/env python3
 # TODO: python2 compatible.
 
-import json
+"""
+Contexts
 
+GET /contexts               - lists all current contexts
+GET /contexts/<name>        - gets info about a context, such as the spark UI url
+POST /contexts/<name>       - creates a new context
+DELETE /contexts/<name>     - stops a context and all jobs running in it. Additionally,
+    you can pass ?force=true to stop a context forcefully. This is equivalent to killing
+    the application from SparkUI (works for spark standalone only).
+PUT /contexts?reset=reboot  - shuts down all contexts and re-loads only the contexts from config.
+    Use ?sync=false to execute asynchronously.
+
+Jobs
+
+Jobs submitted to the job server must implement a SparkJob trait. It has a main runJob method
+which is passed a SparkContext and a typesafe Config object. Results returned by the method
+are made available through the REST API.
+
+GET /jobs                - Lists the last N jobs
+GET /jobs/<jobId>/config - Gets the job configuration
+GET /jobs/<jobId>        - Gets the result or status of a specific job
+POST /jobs               - Starts a new job, use ?sync=true to wait for results
+DELETE /jobs/<jobId>     - Kills the specified job
+"""
+
+import json
+import logging
+import sys
+from functools import wraps
 from urllib import request, parse
 
-url = 'http://10.198.47.106:8090/jobs'
+logger = logging.getLogger("join_framework")
+base_url = "http://10.198.47.106:8090"
 
-queryParams = {
-    'appName': 'join-framework',
-    'classPath': 'spark.jobserver.RunSqlWithOutputJob',
-    'context': 'join-framework-context-0',
-    'sync': 'true'
-}
 
-querystring = parse.urlencode(queryParams)
+def with_response(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        resp = func(*args, **kwargs).read().decode('utf-8')
+        logger.info("Response of {}: {}".format(func.__name__, resp))
+        return resp
+    return wrapper
 
-form = {
-    "sql": "select * from dim_product_daily_item_sku limit 10"
-}
 
-# Make a POST request and read the response
-u = request.urlopen(url + '?' + querystring, data=json.dumps(form).encode("utf-8"))
-resp = u.read().decode('utf-8')
-print(resp)
+class Context:
+    def __init__(self, name="join-framework-context-0"):
+        self.name = name
+
+
+class Operation:
+    def __init__(self, category=""):
+        self.url = base_url + category
+
+
+class ContextOperation(Operation):
+    def __init__(self):
+        super().__init__("/contexts")
+
+    @with_response
+    def list(self):
+        return request.urlopen(self.url)
+
+    @with_response
+    def get_config(self, name):
+        pass
+
+    @with_response
+    def create(self, name):
+        pass
+
+    @with_response
+    def delete(self, name):
+        pass
+
+
+class JobOperation(Operation):
+    def __init__(self):
+        super().__init__("/jobs")
+        self.default_context = Context("join-framework-context-0")
+
+    @with_response
+    def list(self):
+        return request.urlopen(self.url)
+
+    @with_response
+    def get_config(self, id):
+        pass
+
+    @with_response
+    def get_status(self, id):
+        """Get run info of job
+
+        A typical use-case is to loop determine if a async job has finished."""
+        return request.urlopen(self.url + "/" + id)
+
+    @with_response
+    def create(self, context=None):
+        if context is None:
+            context = self.default_context
+        pass
+
+    @with_response
+    def delete(self, id):
+        pass
+
+    @with_response
+    def run_sql(self, sql, context=None, sync="false"):
+        """Run Sql in pre-created context
+
+        It is recommended that you load and cache large dim table first."""
+
+        if context is None:
+            context = self.default_context
+
+        query_params = {
+            # appName is the path variable of uploaded jar.
+            'appName': 'join-framework',
+            'classPath': 'spark.jobserver.RunSqlWithOutputJob',
+            'context': context.name,
+            'sync': sync
+        }
+
+        query_string = parse.urlencode(query_params)
+        logger.debug("Encoded query string: {}".format(query_string))
+
+        form = {
+            "sql": sql
+        }
+
+        # Make a POST request
+        return request.urlopen(self.url + '?' + query_string, data=json.dumps(form).encode("utf-8"))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(name)s: %(message)s',
+        level=logging.INFO)
+
+    context = None if len(sys.argv) < 2 else Context(sys.argv[1])
+
+    context_operation = ContextOperation()
+    context_operation.list()
+
+    job_operation = JobOperation()
+    sql = "select * from dim_product_daily_item_sku limit 10"
+    job_operation.run_sql(sql=sql, context=context)
