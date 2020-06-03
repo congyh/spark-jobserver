3e1eb5e2f07194cbfb715ad02d822d3f7660f60c
congyh <yihao.cong@outlook.com>
2020-06-03 13:34:19 +0800

    Extract run method to generalize job running.

--- job-server-extras/integration/python_intg.py
+++ job-server-extras/integration/python_intg.py
@@ -26,6 +26,7 @@ DELETE /jobs/<jobId>     - Kills the specified job
 
 import json
 import logging
+import sys
 import time
 from functools import partial
 from functools import wraps
@@ -39,7 +40,7 @@ def with_response(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         resp = request.urlopen(func(*args, **kwargs)).read().decode('utf-8')
-        logger.debug("Response of {}: {}".format(func.__name__, resp))
+        logger.debug("Response of [{func_name}]: [{resp}]".format(func_name=func.__name__, resp=resp))
         return resp
 
     return wrapper
@@ -65,12 +66,12 @@ class Context:
         self.name = name
 
 
-class Operation:
+class RestOperation:
     def __init__(self, category=""):
         self.url = base_url + category
 
 
-class ContextOperation(Operation):
+class ContextOperation(RestOperation):
     def __init__(self):
         super().__init__("/contexts")
 
@@ -89,6 +90,7 @@ class ContextOperation(Operation):
     @with_response
     @delete_request
     def delete(self, name):
+        # TODO: Exception handling
         return request.Request(self.url + "/" + name)
 
     @with_response
@@ -97,11 +99,11 @@ class ContextOperation(Operation):
         return request.Request(self.url + "?reset=reboot?sync=false")
 
 
-class JobOperation(Operation):
+class JobOperation(RestOperation):
     def __init__(self):
         super().__init__("/jobs")
         self.last_submit_id = ""
-        self.default_context = Context("join-framework-context-0")
+        self._default_context = Context()
 
     @with_response
     def list(self):
@@ -110,16 +112,16 @@ class JobOperation(Operation):
     @with_response
     def get_info(self, id=""):
         """Get config of job (not running status)"""
-        return request.Request(self.url + "/" + self.__get_job_id(id) + "/config")
+        return request.Request(self.url + "/" + self._get_job_id(id) + "/config")
 
     @with_response
     def get_status(self, id=""):
         """Get run info of job
 
         A typical use-case is to loop determine if a async job has finished."""
-        return request.Request(self.url + "/" + self.__get_job_id(id))
+        return request.Request(self.url + "/" + self._get_job_id(id))
 
-    def __get_job_id(self, id):
+    def _get_job_id(self, id):
         if len(id) > 0:
             return id
         elif len(self.last_submit_id) > 0:
@@ -127,21 +129,11 @@ class JobOperation(Operation):
         else:
             raise Exception("Job id required!")
 
-    @with_response
-    def create(self, context=None):
+    def run(self, query_params, context=None, blocking=True, form={}):
         if context is None:
-            context = self.default_context
-        pass
+            context = self._default_context
 
-    @with_response
-    def delete(self, id=""):
-        return request.Request(self.url + "/" + self.__get_job_id(id))
-
-    def run_sql(self, sql, context=None, blocking=True):
-        if context is None:
-            context = self.default_context
-
-        job_submit_ret_str = self.__run_sql_async(sql, context)
+        job_submit_ret_str = self._run_async(query_params, context, form)
 
         if not blocking:
             return job_submit_ret_str
@@ -150,25 +142,47 @@ class JobOperation(Operation):
             self.last_submit_id = job_id
             job_status = {}
             while True:
-                job_status = json.loads(self.get_status(job_id))
+                job_status = json.loads(self.get_status(id=job_id))
                 running_status = job_status["status"]
                 if running_status != "FINISHED":
                     logger.info("Job id: [{job_id}] not finished yet, now in status: [{job_status}]"
-                                   .format(job_id=job_id, job_status=running_status))
+                                .format(job_id=job_id, job_status=running_status))
                     time.sleep(10)
+                    # TODO: Handle other status.
                 else:
                     break
 
             result = job_status["result"]
             logger.info("Job id: [{job_id}] finished!".format(job_id=job_id))
-            logger.debug("Job id: [{job_id}] result: \n {result}.".format(job_id=job_id, result=result))
+            logger.debug("Job id: [{job_id}] result: \n [{result}].".format(job_id=job_id, result=result))
             return result
 
     @with_response
-    def __run_sql_async(self, sql, context=None):
-        """Run Sql in pre-created context
+    def _run_async(self, query_params, context, form):
+        """Run job in pre-created context
 
         It is recommended that you load and cache large dim table first."""
+        assert query_params is not None
+        assert context is not None
+        assert form is not None
+
+        query_string = parse.urlencode(query_params)
+        logger.debug("Encoded query string: [{query_str}]".format(query_str=query_string))
+
+        if len(form) == 0:
+            return request.Request(self.url + '?' + query_string)
+        else:
+            # Make a POST request
+            return request.Request(self.url + '?' + query_string, data=json.dumps(form).encode("utf-8"))
+
+    @with_response
+    def delete(self, id=""):
+        return request.Request(self.url + "/" + self._get_job_id(id))
+
+    def run_sql(self, sql, context=None, blocking=True):
+        if context is None:
+            context = self._default_context
+
         query_params = {
             # appName is the path variable of uploaded jar.
             'appName': 'join-framework',
@@ -177,15 +191,11 @@ class JobOperation(Operation):
             'sync': "false"
         }
 
-        query_string = parse.urlencode(query_params)
-        logger.debug("Encoded query string: {}".format(query_string))
-
         form = {
             "sql": sql
         }
 
-        # Make a POST request
-        return request.Request(self.url + '?' + query_string, data=json.dumps(form).encode("utf-8"))
+        return self.run(query_params, blocking=blocking, form=form)
 
 
 if __name__ == "__main__":
@@ -193,13 +203,13 @@ if __name__ == "__main__":
         format='%(asctime)s %(levelname)s %(name)s: %(message)s',
         level=logging.DEBUG)
 
-    # context = None if len(sys.argv) < 2 else Context(sys.argv[1])
-    #
+    context = None if len(sys.argv) < 2 else Context(name=sys.argv[1])
+
     context_operation = ContextOperation()
     context_operation.list()
-    #
-    # job_operation = JobOperation()
-    # sql = "select * from dim_product_daily_item_sku limit 10"
-    # job_operation.run_sql(sql=sql, context=context)
 
-    context_operation.delete("sql-context-0")
+    job_operation = JobOperation()
+    sql = "select * from dim_product_daily_item_sku limit 10"
+    job_operation.run_sql(sql=sql, context=context)
+
+    # context_operation.delete("sql-context-0")
