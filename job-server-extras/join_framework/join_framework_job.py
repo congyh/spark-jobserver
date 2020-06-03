28425518eba2ec478dbd54339bd573e921ffa4a7
congyh <yihao.cong@outlook.com>
2020-06-03 20:40:36 +0800

    Add check.

--- job-server-extras/join_framework/join_framework_job.py
+++ job-server-extras/join_framework/join_framework_job.py
@@ -10,20 +10,34 @@ logger = logging.getLogger("join_framework")
 if __name__ == "__main__":
     logging.basicConfig(
         format='%(asctime)s %(levelname)s %(name)s: %(message)s',
-        level=logging.DEBUG)
+        level=logging.INFO)
     context_operation = intg.ContextOperation()
+    logger.info("Start re-booting join-framework.")
 
     # 1. Clear all existing context.
 
     context_operation.clear_all()
-    logger.info("Waiting 30s before reboot...")
+    logger.info("Requested clear all context, waiting 30s before re-create...")
     time.sleep(30)
 
     # 2. Create a new default context.
-    context_operation.create_default()
+    ret_for_create_context = context_operation.create_default()
+    if ret_for_create_context["status"] == "SUCCESS":
+        logger.info("Default context created.")
+    else:
+        err_msg = "Error creating default context!"
+        logger.error(err_msg)
+        raise Exception(err_msg)
 
     # 3. Load dim table and check.
     job_operation = intg.JobOperation()
     job_operation.load_and_cache_table()
+    logger.info("Dim table loaded.")
     ret_for_check_sql = job_operation.run_sql("SELECT * from dim_product_daily_item_sku LIMIT 10")
-    if ret_for_check_sql
+    if ret_for_check_sql["status"] == "FINISHED":
+        logger.info("Cache check passed.")
+        logger.info("join-framework re-booted!")
+    else:
+        err_msg = "Error re-boot join framework! Check spark-jobserver log for detail."
+        logger.error(err_msg)
+        raise Exception(err_msg)
